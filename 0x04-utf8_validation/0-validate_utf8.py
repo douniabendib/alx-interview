7226c93f3e9192b379702de640754b2d04f220a9
douniabendib <bendib.dounia@gmail.com>
2024-04-04 19:10:17 -0700

    done

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,8 +5,10 @@
 
 
 def validUTF8(data):
-    """method that determines if a given
-    data set represents a valid UTF-8 encoding.
+    """
+    method that determines if a given
+    data set represents a 
+    valid UTF-8 encoding.
     """
     byte_count = 0
 
